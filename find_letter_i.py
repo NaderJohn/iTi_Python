6b1c30f1b9bc7aa35f247f6e2b0fcc4e6af0cb15
Nader John <96653232+NaderJohn@users.noreply.github.com>
2025-07-13 21:47:00 +0300

    Delete find_letter_i.py

--- find_letter_i.py
+++
@@ -1,8 +0,0 @@
-def find_letter_i():
-    x=input("Enter any name  ")
-    x=str(x)
-    positions = []
-    for i in range(len(x)):
-        if x[i]=="i":
-            positions.append(i+1)
-    return {"text": x, "positions": positions}
