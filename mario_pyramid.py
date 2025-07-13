e3f1d16e35a8877e0cd2c9a534513fcbb2e35cf2
Nader John <96653232+NaderJohn@users.noreply.github.com>
2025-07-13 21:40:27 +0300

    Delete mario_pyramid.py

--- mario_pyramid.py
+++
@@ -1,8 +0,0 @@
-def mario_pyramid():
-    lst=[' ',' ',' ',' ',' ',' ']
-    pyramid = []
-    for i in range(6):
-        lst.remove(' ')
-        lst.append('*')
-        pyramid.append(''.join(lst))
-    return '\n'.join(pyramid)
